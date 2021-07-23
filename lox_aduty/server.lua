7e1c0a8439b6767dada4c423b674fec08245d362
Marlox4 <70572904+Marlox4@users.noreply.github.com>
2021-07-23 07:44:02 +0200

    Update server.lua

--- lox_aduty/server.lua
+++ lox_aduty/server.lua
@@ -24,19 +24,19 @@ end)
 
 RegisterCommand("aduty", function(source, args)
     local xPlayer = ESX.GetPlayerFromId(source)
-    if xPlayer.getGroup() == "pl" or xPlayer.getGroup() == "manager" or xPlayer.getGroup() == "superadmin" or xPlayer.getGroup() == "admin" or xPlayer.getGroup() == "mod" or xPlayer.getGroup() == "support" or xPlayer.getGroup() == "_dev" then
+    if xPlayer.getGroup() == "pl" or xPlayer.getGroup() == "stvpl" or xPlayer.getGroup() == "manager" or xPlayer.getGroup() == "teamleitung" or xPlayer.getGroup() == "superadmin" or xPlayer.getGroup() == "admin" or xPlayer.getGroup() == "supermod" or xPlayer.getGroup() == "mod" or xPlayer.getGroup() == "supersupport" or xPlayer.getGroup() == "support" or xPlayer.getGroup() == "_dev" then
         TriggerClientEvent('toggleAduty', source)
     else
-        TriggerClientEvent('lox_notify', source, "#1df5fc", "Admindienst", "Du hast keine Rechte.")
+        TriggerClientEvent(Config.Notification, source, "#1df5fc", "Admindienst", "Du hast keine Rechte.")
     end
 end, false)
 
 RegisterCommand("ad", function(source, args)
     local xPlayer = ESX.GetPlayerFromId(source)
-    if xPlayer.getGroup() == "pl" or xPlayer.getGroup() == "manager" or xPlayer.getGroup() == "superadmin" or xPlayer.getGroup() == "admin" or xPlayer.getGroup() == "mod" or xPlayer.getGroup() == "support" or xPlayer.getGroup() == "_dev" then
+    if xPlayer.getGroup() == "pl" or xPlayer.getGroup() == "stvpl" or xPlayer.getGroup() == "manager" or xPlayer.getGroup() == "teamleitung" or xPlayer.getGroup() == "superadmin" or xPlayer.getGroup() == "admin" or xPlayer.getGroup() == "supermod" or xPlayer.getGroup() == "mod" or xPlayer.getGroup() == "supersupport" or xPlayer.getGroup() == "support" or xPlayer.getGroup() == "_dev" then
         TriggerClientEvent('toggleAduty', source)
     else
-        TriggerClientEvent('lox_notify', source, "#1df5fc", "Admindienst", "Du hast keine Rechte.")
+        TriggerClientEvent(Config.Notification, source, "#1df5fc", "", "Du hast keine Rechte.")
     end
 end, false)
 
@@ -46,18 +46,18 @@ end, false)
 TriggerEvent("es:addGroupCommand", "ad", "superadmin", function(source, args)
     TriggerClientEvent("toggleAduty", source)
 end, function(source, args)
-    TriggerEvent('lox_notify', "#1df5fc", "ENTWICKLER-MODUS", "Keine Rechte.")
+    TriggerEvent(Config.Notification, "#1df5fc", "ENTWICKLER-MODUS", "Keine Rechte.")
 end)
 
 TriggerEvent("es:addGroupCommand", "ad", "mod", function(source, args)
     TriggerClientEvent("toggleAduty", source)
 end, function(source, args)
-    TriggerEvent('lox_notify', "#1df5fc", "ADMINISTRATION", "Keine Rechte.")
+    TriggerEvent(Config.Notification, "#1df5fc", "ADMINISTRATION", "Keine Rechte.")
 end)
 
 TriggerEvent("es:addGroupCommand", "ad", "admin", function(source, args)
     TriggerClientEvent("toggleAduty", source)
 end, function(source, args)
-    TriggerEvent('lox_notify', "#1df5fc", "ADMINISTRATION", "Keine Rechte.")
+    TriggerEvent(Config.Notification, "#1df5fc", "ADMINISTRATION", "Keine Rechte.")
 end)
 
