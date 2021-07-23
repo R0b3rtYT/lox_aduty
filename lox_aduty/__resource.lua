8642198b4476a33893e629ad95e1824c535fac32
Marlox4 <70572904+Marlox4@users.noreply.github.com>
2021-07-23 07:54:21 +0200

    Update __resource.lua

--- lox_aduty/__resource.lua
+++ lox_aduty/__resource.lua
@@ -2,6 +2,11 @@ resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
 ui_page 'files/index.html'
 
+description "Fivem Aduty mit keinen bugs, Das beste NoClip"
+author 'Marlox <https://github.com/Marlox4>'
+version '1.0'
+
+
 files {
     'files/index.html',
     'files/app.js',
